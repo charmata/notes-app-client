a2665ba50f2905f3958a042d042c835fb092cd14
Chris Armata <chris.armata@gmail.com>
2019-09-05 12:41:57 -0400

    Config environment

--- src/config.js
+++ src/config.js
@@ -1,17 +1,42 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-8phiikzuwnnd"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://aknjk297y9.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    USER_POOL_ID: "us-east-1_upz0k2ziN",
+    APP_CLIENT_ID: "1hurli1egc2dj66134n2o1tqvp",
+    IDENTITY_POOL_ID: "us-east-1:dcc57410-ce64-44f8-8431-2d898c58ad5d"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-ciox068bo7ag"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://lxsdppc6w2.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_lle5x2RPx",
+    APP_CLIENT_ID: "76bq61r0cp7hj22u7e9s86osa4",
+    IDENTITY_POOL_ID: "us-east-1:fd2fa771-2974-41b8-a59e-c35f7ae64384"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
